c76e425c535f424008f2ac0d23f3f4d47256c795
ferni232 <fernandorudnevich.inedita@gmail.com>
2024-01-15 22:58:48 -0300

    puse class

--- main.js
+++ main.js
@@ -5,13 +5,15 @@
 // }
 // console.log(persona1)
 
-function Mascotas(nombre,tipo,dueno,edad,castrado){
-    this.nombre=nombre;
-    this.tipo=tipo;
-    this.dueno=dueno;
-    this.edad=edad;
-    this.castrado=castrado;
+class Mascotas {
+    constructor(nombre, tipo, dueno, edad, castrado) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.dueno = dueno;
+        this.edad = edad;
+        this.castrado = castrado;
 
+    }
 }
 
 
@@ -21,13 +23,13 @@ const mila = new Mascotas ("Mila","perro","fulanito gonzales",3,false);
 console.log(mila)
 
 
-const crearMascota =()=>{
-    let nombreMascota = prompt("ingresa el nombre de tu mascoto");
-    let tipoMascota = prompt("Que tipo de mascota es?");
-    let nombreDueno = prompt("Como te llamas?");
-    let edadMascota = Number( prompt("Que edad tiene"));
-    let estaCastrado = prompt("esta castrado?");
+// const crearMascota =()=>{
+//     let nombreMascota = prompt("ingresa el nombre de tu mascoto");
+//     let tipoMascota = prompt("Que tipo de mascota es?");
+//     let nombreDueno = prompt("Como te llamas?");
+//     let edadMascota = Number( prompt("Que edad tiene"));
+//     let estaCastrado = prompt("esta castrado?");
 
-    const mascota2 = new Mascotas(nombreMascota,tipoMascota,nombreDueno,edadMascota,estaCastrado);
-    console.log(mascota2);
-}
+//     const mascota2 = new Mascotas(nombreMascota,tipoMascota,nombreDueno,edadMascota,estaCastrado);
+//     console.log(mascota2);
+// }
